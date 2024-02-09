8b511e2646723fa6fba03d0089b2f10c66daf8e2
yash1 <yash12012002@gmail.com>
2024-02-09 12:28:10 +0530

    comment added on CommentRepo

--- src/main/java/com/veersablog/BlogApp/repositories/CommentRepo.java
+++ src/main/java/com/veersablog/BlogApp/repositories/CommentRepo.java
@@ -3,5 +3,6 @@ package com.veersablog.BlogApp.repositories;
 import com.veersablog.BlogApp.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*Comment repo*/
 public interface CommentRepo extends JpaRepository<Comment , Integer> {
 }
